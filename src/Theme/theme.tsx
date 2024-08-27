4afddf86d27710d49a640039f9b7c497a9f1831f
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-08-27 05:49:59 +0100

    implemented banner and sub-banner

---
+++ src/Theme/theme.tsx
@@ -0,0 +1,13 @@
+import type { Theme } from 'theme-ui'
+
+export const theme: Theme = {
+  fonts: {
+    heading: 'Mulish'
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#444142',
+    secondary: '#e60f3d'
+  },
+}
