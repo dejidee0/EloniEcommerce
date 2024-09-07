e1a3f4813d07db81f88ab5c9f750b2c874f3e198
Bennyyoung <benjamineffiong124@yahoo.com>
2024-09-07 16:15:55 +0100

    redsolved conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,17 @@ import SignUp from "@/pages/SignUp/SignUp";
 import UsersDashboard from "@/pages/Dashboard/UsersDashboard/UsersDashboard";
 import AdminDashboard from "@/pages/Dashboard/AdminDashboard/AdminDashboard";
 import PrivateRoute from "@/components/PrivateRoute/PrivateRoute";
-<<<<<<< HEAD
 import Otp from "@/pages/Otp/Otp.tsx";
-=======
-import OtpPage from "./pages/OtpPage/OtpPage.tsx";
->>>>>>> 3768d45da8e1d30bdb0dd99e9ddf4257d2407099
 
 export default function App() {
   return (
     <ThemeUIProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
         <Route path='/' element={<UsersDashboard />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path="/otp" element={<Otp />} />
-=======
-          <Route path="/" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/otp-page" element={<OtpPage />} />
->>>>>>> 3768d45da8e1d30bdb0dd99e9ddf4257d2407099
           <Route
             path="/users-dashboard"
             element={
