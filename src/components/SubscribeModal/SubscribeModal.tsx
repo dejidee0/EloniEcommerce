222db9ee87f45b4a19a4834ad3a4cc5aa42def8e
youngbenjie <benjamin.effiong@rekitadvisory.com>
2024-09-12 09:57:45 +0100

    fix
    SubscribeModal conflict

--- src/components/SubscribeModal/SubscribeModal.tsx
+++ src/components/SubscribeModal/SubscribeModal.tsx
@@ -44,11 +44,7 @@ const SubscribeModal: React.FC = () => {
               left: "50%",
               transform: "translate(-50%, -50%)",
               backgroundColor: "white",
-<<<<<<< HEAD
-              padding: ["30px", "40px", "50px"],
-=======
               padding: ["55px", "50px", "50px"],
->>>>>>> e6a03c8968f0c4a9b76ff0ff0fea303f24943f7b
               borderRadius: "12px",
               zIndex: 11,
               textAlign: ["center", "center"],
@@ -83,13 +79,6 @@ const SubscribeModal: React.FC = () => {
               src="/Telegram logo.png"
               alt="Telegram Icon"
               sx={{
-<<<<<<< HEAD
-                width: "35%",
-                marginRight: ["0", "5px"],
-                marginBottom: ["20px", "0"],
-                display: ["none", "block"],
-                borderRadius: "10px",
-=======
                 // width: "50%",
                 width: '35%',
                 height: '230px',
@@ -98,7 +87,6 @@ const SubscribeModal: React.FC = () => {
                 display: ["none", "block"],
                 marginBottom: ["20px", "0"],
                 borderRadius: "10px"
->>>>>>> e6a03c8968f0c4a9b76ff0ff0fea303f24943f7b
               }}
             />
 
